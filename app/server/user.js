3292e762173252e74299f80716d6c3e105a85011
fragaeverton <evertonfraga90@hotmail.com>
2023-08-08 00:01:42 +0100

    fixes

--- app/server/user.js
+++ app/server/user.js
@@ -6,9 +6,4 @@ const  {createUser, createUserAddress}  = require('./queries');
 
 //userRouter.get('/all', getAllProducts);
 userRouter.post('/user', createUser);
-<<<<<<< HEAD
 userRouter.post('/address', createUserAddress);
-
-=======
-userRouter.post('/address', createUserAddress);
->>>>>>> f3ec8b851d91c9d25ef3f00c056479760d16ae10
